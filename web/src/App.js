98b29a10eacac313df195a215b5d4f9b48699da7
Jacob Ho <clh161@gmail.com>
2021-05-03 16:03:44 +0100

    https

--- web/src/App.js
+++ web/src/App.js
@@ -18,7 +18,7 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="http://devkits.net/">
+      <Link color="inherit" href="https://devkits.net/">
         Devkits
       </Link>{' '}
       {new Date().getFullYear()}
