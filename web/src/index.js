4398fc7e75ecf6d7171361891c14157b2b9780c9
Jacob Ho <clh161@gmail.com>
2021-05-03 20:57:21 +0100

    flow typed

--- web/src/index.js
+++ web/src/index.js
@@ -11,30 +11,33 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import TimestampConverter from './page/TimestampConverter';
 import Home from './page/Home';
 import CaseTypeConverter from './page/CaseTypeConverter';
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <div>
-        <Switch>
-          <App>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/html-encoding">
-              <HTMLEncoder />
-            </Route>
-            <Route path="/unix-timestamp-converter">
-              <TimestampConverter />
-            </Route>{' '}
-            <Route path="/case-type-converter">
-              <CaseTypeConverter />
-            </Route>
-          </App>
-        </Switch>
-      </div>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const root = document.getElementById('root');
+root != null &&
+  ReactDOM.render(
+    <React.StrictMode>
+      <Router>
+        <div>
+          <Switch>
+            <App>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/html-encoding">
+                <HTMLEncoder />
+              </Route>
+              <Route path="/unix-timestamp-converter">
+                <TimestampConverter />
+              </Route>{' '}
+              <Route path="/case-type-converter">
+                <CaseTypeConverter />
+              </Route>
+            </App>
+          </Switch>
+        </div>
+      </Router>
+    </React.StrictMode>,
+    root
+  );
 
 reportWebVitals();
