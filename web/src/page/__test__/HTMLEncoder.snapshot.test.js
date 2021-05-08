6bdf2c1fa28558114407692673e1b13ad970c169
Jacob Ho <clh161@gmail.com>
2021-05-08 02:55:07 +0100

    update test

--- web/src/page/__test__/HTMLEncoder.snapshot.test.js
+++ web/src/page/__test__/HTMLEncoder.snapshot.test.js
@@ -4,9 +4,20 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import HTMLEncoder from '../HTMLEncoder';
 
-it('renders correctly', () => {
+it('Init with decoded text', () => {
   const tree = renderer
     .create(<HTMLEncoder initDecodedText={'Example: < > " \\\' &'} />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('Init with encoded text', () => {
+  const tree = renderer
+    .create(
+      <HTMLEncoder
+        initEncodedText={'Example: &lt; &gt; &quot; \\&apos; &amp;'}
+      />
+    )
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
