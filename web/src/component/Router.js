8c869d232b0efd335839adc79b587c86a897d0e2
Jacob Ho <clh161@gmail.com>
2021-05-11 02:28:44 +0100

    refactor

--- web/src/component/Router.js
+++ web/src/component/Router.js
@@ -1,5 +1,10 @@
 // @flow strict
 
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import CodeIcon from '@material-ui/icons/Code';
+import HttpIcon from '@material-ui/icons/Http';
+import TextFieldsIcon from '@material-ui/icons/TextFields';
+import ViewComfyIcon from '@material-ui/icons/ViewComfy';
 import type { Node } from 'react';
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
@@ -11,21 +16,57 @@ import HTMLEncoder from '../page/HTMLEncoder';
 import TimestampConverter from '../page/TimestampConverter';
 import URLEncoder from '../page/URLEncoder';
 
-type Config = {
-  path: string,
-  component: Node,
-};
+type Config =
+  | {
+      isHidden: true,
+      path: string,
+      component: Node,
+    }
+  | {
+      isHidden: false,
+      path: string,
+      component: Node,
+      icon: Node,
+      name: string,
+    };
 
 export const CONFIGS: Array<Config> = [
   {
+    isHidden: false,
     path: '/html-encoding',
+    name: 'HTML Encoder',
+    icon: <CodeIcon />,
     component: <HTMLEncoder initDecodedText={'Example: < > " \\\\\' &'} />,
   },
-  { path: '/url-encoding', component: <URLEncoder /> },
-  { path: '/unix-timestamp-converter', component: <TimestampConverter /> },
-  { path: '/case-type-converter', component: <CaseTypeConverter /> },
-  { path: '/csv-editor', component: <CSVEditor /> },
-  { path: '/', component: <Home /> },
+  {
+    isHidden: false,
+    path: '/url-encoding',
+    component: <URLEncoder />,
+    icon: <HttpIcon />,
+    name: 'URL Encoder',
+  },
+  {
+    isHidden: false,
+    path: '/unix-timestamp-converter',
+    name: 'Unix Timestamp Converter',
+    component: <TimestampConverter />,
+    icon: <AccessTimeIcon />,
+  },
+  {
+    isHidden: false,
+    path: '/case-type-converter',
+    component: <CaseTypeConverter />,
+    name: 'Case Type Converter',
+    icon: <TextFieldsIcon />,
+  },
+  {
+    isHidden: false,
+    path: '/csv-editor',
+    component: <CSVEditor />,
+    icon: <ViewComfyIcon />,
+    name: 'CSV Editor',
+  },
+  { isHidden: true, path: '/', component: <Home /> },
 ];
 
 export default function Router(): Node {
