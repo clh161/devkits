658d250278d8bb50790910efcb456a63e164005f
Jacob Ho <clh161@gmail.com>
2021-05-11 04:45:26 +0100

    done

--- web/src/component/Router.js
+++ web/src/component/Router.js
@@ -9,7 +9,8 @@ import type { Node } from 'react';
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import CaseTypeConverter, { CASE_TYPES } from '../page/CaseTypeConverter';
+import { CASE_TYPES } from '../page/CASE_TYPES';
+import CaseTypeConverter from '../page/CaseTypeConverter';
 import CSVEditor from '../page/CSVEditor';
 import Home from '../page/Home';
 import HTMLEncoder from '../page/HTMLEncoder';
