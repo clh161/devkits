e092e65b4aa52245f6b77d333d6a3e7bf08b2ba3
Jacob Ho <clh161@gmail.com>
2021-05-11 02:08:25 +0100

    refactor

--- web/src/component/Router.js
+++ web/src/component/Router.js
@@ -11,28 +11,34 @@ import HTMLEncoder from '../page/HTMLEncoder';
 import TimestampConverter from '../page/TimestampConverter';
 import URLEncoder from '../page/URLEncoder';
 
+type Config = {
+  path: string,
+  component: Node,
+};
+
+export const CONFIGS: Array<Config> = [
+  {
+    path: '/html-encoding',
+    component: <HTMLEncoder initDecodedText={'Example: < > " \\\\\' &'} />,
+  },
+  { path: '/url-encoding', component: <URLEncoder /> },
+  { path: '/unix-timestamp-converter', component: <TimestampConverter /> },
+  { path: '/case-type-converter', component: <CaseTypeConverter /> },
+  { path: '/csv-editor', component: <CSVEditor /> },
+  { path: '/', component: <Home /> },
+];
+
 export default function Router(): Node {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/html-encoding">
-          <HTMLEncoder initDecodedText={'Example: < > " \\\\\' &'} />
-        </Route>
-        <Route path="/url-encoding">
-          <URLEncoder />
-        </Route>
-        <Route path="/unix-timestamp-converter">
-          <TimestampConverter />
-        </Route>
-        <Route path="/case-type-converter">
-          <CaseTypeConverter />
-        </Route>
-        <Route path="/csv-editor">
-          <CSVEditor />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
+        {CONFIGS.map((config) => {
+          return (
+            <Route key={config.path} path={config.path}>
+              {config.component}
+            </Route>
+          );
+        })}
       </Switch>
     </BrowserRouter>
   );
