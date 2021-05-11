885545ff8a290f235f697bdca4e4b9004c89fe40
Jacob Ho <clh161@gmail.com>
2021-05-11 04:33:22 +0100

    add test

---
+++ web/src/page/__test__/App.snapshot.test.js
@@ -0,0 +1,13 @@
+// @flow
+
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import renderer from 'react-test-renderer';
+
+import App from '../../App';
+
+it('Init with default', () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
+  expect(Helmet.peek()).toMatchSnapshot();
+});
