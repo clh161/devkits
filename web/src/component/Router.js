2810f6894efd1b1353457e17f14987d2bf32ddd4
Jacob Ho <clh161@gmail.com>
2021-05-11 01:30:41 +0100

    add url decoder

--- web/src/component/Router.js
+++ web/src/component/Router.js
@@ -5,9 +5,10 @@ import Home from '../page/Home';
 import HTMLEncoder from '../page/HTMLEncoder';
 import TimestampConverter from '../page/TimestampConverter';
 import CaseTypeConverter from '../page/CaseTypeConverter';
-import React from 'react';
 import type { Node } from 'react';
+import React from 'react';
 import CSVEditor from '../page/CSVEditor';
+import URLEncoder from '../page/URLEncoder';
 
 export default function Router(): Node {
   return (
@@ -15,6 +16,9 @@ export default function Router(): Node {
       <Switch>
         <Route path="/html-encoding">
           <HTMLEncoder initDecodedText={'Example: < > " \\\\\' &'} />
+        </Route>
+        <Route path="/url-encoding">
+          <URLEncoder />
         </Route>
         <Route path="/unix-timestamp-converter">
           <TimestampConverter />
