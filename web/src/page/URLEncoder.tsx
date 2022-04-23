e4cc12f45c2ee60f918e7b00af4bcf5c70f83cf6
clh161 <clh16@gmail.com>
2022-04-24 00:45:01 +0100

    [flow-to-ts] URLEncoder.tsx

--- web/src/page/URLEncoder.tsx
+++ web/src/page/URLEncoder.tsx
@@ -1,18 +1,16 @@
-// @flow strict
 import { Divider, Grid } from '@material-ui/core';
-import type { Node } from 'react';
-import React, { useState } from 'react';
-
+import React, { ReactElement, useState } from 'react';
 const DEFAULT_URL =
   'https://devkits.net/json=%7B%0A%20%20%22user%22:%20%7B%0A%20%20%20%20%22id%22:%201,%0A%20%20%20%20%22name%22:%20%22devkits%22%0A%20%20%7D%0A%7D%0A';
 const DECODED_URL = decodeURIComponent(DEFAULT_URL);
-export default function URLEncoder(): Node {
+export default function URLEncoder(): ReactElement {
   const [encodedURL, setEncodedURL] = useState<string>(DEFAULT_URL);
   const [decodedURL, setDecodedURL] = useState<string>(DECODED_URL);
 
   function onEncodedURLChange(event) {
     const { value } = event.target;
     setEncodedURL(value);
+
     try {
       setDecodedURL(decodeURIComponent(value));
     } catch (_) {
@@ -32,7 +30,10 @@ export default function URLEncoder(): Node {
         <textarea
           onChange={onEncodedURLChange}
           placeholder="Encoded URL"
-          style={{ width: '100%', minHeight: 160 }}
+          style={{
+            width: '100%',
+            minHeight: 160,
+          }}
           value={encodedURL}
         />
       </Grid>
@@ -41,7 +42,10 @@ export default function URLEncoder(): Node {
         <textarea
           onChange={onDecodedURLChange}
           placeholder="Decoded URL"
-          style={{ width: '100%', minHeight: 160 }}
+          style={{
+            width: '100%',
+            minHeight: 160,
+          }}
           value={decodedURL}
         />
       </Grid>
