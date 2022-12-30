c6a9fe283c399299815d95e8661af5bd42b9d3f6
Jacob Ho <clh161@gmail.com>
2022-12-30 10:59:33 +0000

    validate json

--- web/src/page/JsonToSchemaPage.tsx
+++ web/src/page/JsonToSchemaPage.tsx
@@ -1,32 +1,39 @@
-import { Grid, TextField } from '@mui/material';
+import { Alert, Stack, TextField } from '@mui/material';
 import React, { ReactElement, useState } from 'react';
-type Props = {
-  initDecodedText?: string;
-  initEncodedText?: string;
-};
 
-export default function JsonToSchemaPage({}: Props): ReactElement {
+export default function JsonToSchemaPage(): ReactElement {
   const [json, setJson] = useState<string>('{}');
+  const jsonParsingError = getJsonPrasingError(json);
 
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <TextField
-          label='Json schema'
-          maxRows={15}
-          minRows={5}
-          multiline
-          onChange={(event) => {
-            const { value } = event.target;
-            setJson(value);
-          }}
-          placeholder='Json schema'
-          style={{
-            width: '100%',
-          }}
-          value={json}
-        />
-      </Grid>
-    </Grid>
+    <Stack spacing={1}>
+      <TextField
+        label='Json schema'
+        maxRows={15}
+        minRows={5}
+        multiline
+        onChange={(event) => {
+          const { value } = event.target;
+          setJson(value);
+        }}
+        placeholder='Json schema'
+        style={{
+          width: '100%',
+        }}
+        value={json}
+      />
+      {jsonParsingError != null && (
+        <Alert color='error'>{jsonParsingError}</Alert>
+      )}
+    </Stack>
   );
+}
+
+function getJsonPrasingError(json: string): string | null {
+  try {
+    JSON.parse(json);
+  } catch (e) {
+    return e.message;
+  }
+  return null;
 }
