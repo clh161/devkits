0cd792c2b03503c8187cc417bde677c0744b5f2c
Jacob Ho <clh161@gmail.com>
2022-12-30 11:42:03 +0000

    fix tab

--- web/src/json-converter/JsonToKotlin.ts
+++ web/src/json-converter/JsonToKotlin.ts
@@ -6,18 +6,16 @@ type KotlinClass = string;
 
 export function getKotlinClass(
   json: object | Array<object>,
-  className: string,
-  tabCount = 1
+  className: string
 ): KotlinClass[] {
   const codeClassStart = `data class ${className}(`;
-  const codeTab = Array.from({ length: tabCount + 1 }).join(TAB);
   const fields = [];
   const nestedClasses: KotlinClass[] = [];
 
   if (typeof json === 'object') {
     const keys = Object.keys(json);
     for (const key of keys) {
-      const codeField = `${codeTab}val ${key}`;
+      const codeField = `${TAB}val ${key}`;
       const value = json[key];
       if (typeof value === 'object') {
         const valueClassName = CASE_TYPES.find(
@@ -27,11 +25,7 @@ export function getKotlinClass(
           valueClassName[0].toUpperCase() + valueClassName.slice(1);
         fields.push(`${codeField}: ${valueClassNameCapital}`);
 
-        const nestedClass = getKotlinClass(
-          value,
-          valueClassNameCapital,
-          tabCount + 1
-        );
+        const nestedClass = getKotlinClass(value, valueClassNameCapital);
 
         nestedClasses.push(...nestedClass);
       } else {
