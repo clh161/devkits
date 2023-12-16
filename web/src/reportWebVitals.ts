31d280ffa0e3588722dc58255e15457a73f28767
Jacob Ho <clh161@gmail.com>
2023-12-16 17:34:32 +0000

    Refactor reportWebVitals function to import web-vitals/base instead of web-vitals

--- web/src/reportWebVitals.ts
+++ web/src/reportWebVitals.ts
@@ -1,14 +1,17 @@
-import { ReportHandler } from 'web-vitals';
+import {
+  getCLS,
+  getFID,
+  getLCP,
+  getTTFB,
+  ReportHandler,
+} from 'web-vitals/base';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+  if (onPerfEntry && typeof onPerfEntry === 'function') {
+    getCLS(onPerfEntry);
+    getFID(onPerfEntry);
+    getLCP(onPerfEntry);
+    getTTFB(onPerfEntry);
   }
 };
 
