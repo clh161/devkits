21fe49e320b92d6465652dc6e26c60962264c26b
Jacob Ho <clh161@gmail.com>
2023-12-16 01:34:56 +0000

    Refactor Page component to use destructuring

--- web/src/page/Page.tsx
+++ web/src/page/Page.tsx
@@ -3,16 +3,20 @@ import React, { ReactElement } from 'react';
 
 import HTMLMeta from '../component/HTMLMeta';
 import { RouterConfig } from '../component/RouterConfig';
+
 type Props = {
   children: ReactElement;
   pageConfig: RouterConfig;
 };
+
 export default function Page({ children, pageConfig }: Props): ReactElement {
+  const { title, description } = pageConfig;
+
   return (
     <Stack sx={{ p: 2 }}>
       <HTMLMeta pageConfig={pageConfig} />
       <Typography component='h1' variant='h4'>
-        {pageConfig.title}
+        {title}
       </Typography>
       <Typography
         component='h2'
@@ -20,7 +24,7 @@ export default function Page({ children, pageConfig }: Props): ReactElement {
         sx={{ mb: 2 }}
         variant='subtitle2'
       >
-        {pageConfig.description}
+        {description}
       </Typography>
       {children}
     </Stack>
